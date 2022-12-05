ca08f8601a6660ca90005f95102669135b13c1af
Guille-Rubio <guillermorubiog@gmail.com>
2022-12-05 12:32:48 +0100

    FIX index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const app = require('express')();
+const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
-  res.send('Hello World!')
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/hello-world', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+app.get('/api/hello', (req, res)=>{
+  res.status(200).json({msg:"Hello World!"})
+})
+
+module.exports = app;
