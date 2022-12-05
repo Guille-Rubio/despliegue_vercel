bd0d71f051b03361951d9a5222175a4eb8398af5
Guille-Rubio <guillermorubiog@gmail.com>
2022-12-05 12:46:07 +0100

    FIX index.js to dev.to tutorial

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+app.use(express.json({ extended: false }));
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
 
-app.get('/api/hello', (req, res)=>{
-  res.status(200).json({msg:"Hello World!"})
+
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: "Hello World" });
 })
 
-module.exports = app;
+
+app.listen(PORT, () => console.log(`Server is running in PORT ${PORT}`));
+
+
+
+
