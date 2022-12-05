e268279c452826bf5c6d3a860aa50f286e199240
Guille-Rubio <guillermorubiog@gmail.com>
2022-12-05 12:23:29 +0100

    ADD start script

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+app.get('/hello-world', (req, res) => {
+  res.send('Hello World!')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
