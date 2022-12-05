aa9a5dee774a85712e3a453a0bbd1c16eadf3f51
Guille-Rubio <guillermorubiog@gmail.com>
2022-12-05 12:51:51 +0100

    ADD route /hello

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.status(200).json({ msg: "Hello World" });
 })
 
+app.get('/hello', (req, res) => {
+  res.status(200).json({ msg: "route to /hello is ok" });
+})
+
 
 app.listen(PORT, () => console.log(`Server is running in PORT ${PORT}`));
 
