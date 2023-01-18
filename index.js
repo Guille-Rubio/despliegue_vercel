ed00cf185f8deac15f551fea5c19677e70c9fa8e
Guille-Rubio <guillermorubiog@gmail.com>
2023-01-18 16:21:32 +0100

    ADD require('puppeteer')

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 require('pg');
+require('puppeteer');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
