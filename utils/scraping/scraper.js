451e291fc1f1bab66731a1649170111688573c75
Guille-Rubio <guillermorubiog@gmail.com>
2023-01-25 00:36:08 +0100

    UPDATED node version to ^v14

--- utils/scraping/scraper.js
+++ utils/scraping/scraper.js
@@ -24,7 +24,7 @@ const getPictureUrl = async () => {
         options = {
             args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
             defaultViewport: chrome.defaultViewport,
-            executablePath: "/tmp/chromium",/* await chrome.executablePath */
+            executablePath: await chrome.executablePath,
             headless: true,
             ignoreHTTPSErrors: true,
         };
